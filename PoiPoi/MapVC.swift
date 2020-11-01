91fa322053ab5a752d7ed7c257b825300b913753
Robert Ahlberg <rob.ahlberg@gmail.com>
2020-11-01 21:15:43 +0100

    Add poi annotations

--- PoiPoi/MapVC.swift
+++ PoiPoi/MapVC.swift
@@ -12,6 +12,11 @@ class MapVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
   @IBOutlet weak var mapView: MKMapView!
   
   var locationManager = CLLocationManager()
+  var initialMapPosition: CLLocationCoordinate2D? {
+    didSet {
+      mapCenterArea(coordinate: initialMapPosition!)
+    }
+  }
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -20,19 +25,51 @@ class MapVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     locationManager.delegate = self
     locationManager.startUpdatingLocation()
     
+    mapView.showsUserLocation = true
   }
   
   func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
     if let lastLocation = locations.last {
-      let coordinate = lastLocation.coordinate
-      let viewRegion = MKCoordinateRegion(center: coordinate, latitudinalMeters: 500, longitudinalMeters: 500)
-      mapView.setCenter(coordinate, animated: true)
-      mapView.setRegion(viewRegion, animated: true)
+      
+      // Only set once
+      if initialMapPosition == nil {
+        initialMapPosition = lastLocation.coordinate
+      }
     }
+  }
+  
+  func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+    guard annotation is MKPointAnnotation else { return nil }
+    
+    let identifier = "Annotation"
+    var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+    
+    if annotationView == nil {
+      annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+      annotationView?.canShowCallout = true
+    } else {
+      annotationView?.annotation = annotation
+    }
+    
+    return annotationView
   }
   
   func mapViewDidFinishLoadingMap(_ mapView: MKMapView) {
     print("ok")
+    
+    // Populate annotations
+    for location in State.shared.poiLocations {
+      let annotation = MKPointAnnotation()
+      annotation.title = location.name
+      annotation.coordinate = CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)
+      mapView.addAnnotation(annotation)
+    }
+  }
+  
+  private func mapCenterArea(coordinate: CLLocationCoordinate2D) {
+    let viewRegion = MKCoordinateRegion(center: coordinate, latitudinalMeters: 500, longitudinalMeters: 500)
+    mapView.setCenter(coordinate, animated: false)
+    mapView.setRegion(viewRegion, animated: true)
   }
   
   
